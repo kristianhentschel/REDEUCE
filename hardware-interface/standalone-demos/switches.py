f1982cbd37e38aabcc247b9e498f58b6f52f2fd6
Kristian Hentschel <Kristian.Hentschel@glasgow.ac.uk>
2016-08-31 17:25:58 +0100

    add more to switches script - still untested.

--- hardware-interface/standalone-demos/switches.py
+++ hardware-interface/standalone-demos/switches.py
@@ -1,10 +1,12 @@
 import smbus
 import RPi.GPIO as GPIO
+import time
 
-i2c_base_address = 0x00 # TODO I2C address of first IO expander
+i2c_base_address = 0x20 # I2C address of first IO expander
 i2c_bus = 0
 i2c_enable_pins = [5, 6]
 
+# MCP 23017 register map
 MCP_REG_IODIRA      = 0x00
 MCP_REG_IODIRB      = 0x01
 MCP_REG_IPOLA       = 0x02
@@ -28,9 +30,20 @@ MCP_REG_GPIOB       = 0x13
 MCP_REG_OLATA       = 0x14
 MCP_REG_OLATB       = 0x15
 
-i2c = open_i2c()
+i2c = None
 
 def main():
+    global i2c
+    i2c = open_i2c()
+    for pin in i2c_enable_pins:
+        mcp_initialize(pin)
+
+    while True:
+         data = read_all_ports()
+         for byte in data:
+            print "%b" % byte
+         print "\n"
+         time.sleep(1000)
 
 def open_i2c():
     GPIO.setMode(GPIO.BCM)
@@ -49,15 +62,25 @@ def i2c_read_word(i2c, enable_pin, address, register):
     return i2c.read_byte_data(i2c_base_address + address, register)
 
 def i2c_select_group(enable_pin):
-    for pin in i2c_enable_ins():
+    for pin in i2c_enable_pins:
         if pin == enable_pin:
-            GPIO.output(pin, GPIO.LOW)
-        else:
             GPIO.output(pin, GPIO.HIGH)
+        else:
+            GPIO.output(pin, GPIO.LOW)
 
-def mcp_initialize():
-    
-    i2c_write_byte_data(i2c, 
+def read_all_ports():
+    data = []
+    for pin in i2c_enable_pins:
+        for i in range(8): # TODO hardcoded number of devices per enable pin
+            address = i2c_base_address + i
+            data += i2c_read_word(i2c, pin, address, MCP_REG_GPIOA)
+            data += i2c_read_word(i2c, pin, address, MCP_REG_GPIOB)
+
+def mcp_initialize(pin, address):
+    i2c_write_byte_data(i2c, pin, address, MCP_REG_IODIRA, 0xF)
+    i2c_write_byte_data(i2c, pin, address, MCP_REG_IODIRB, 0xF)
+    i2c_write_byte_data(i2c, pin, address, MCP_REG_GPPUA, 0xF)
+    i2c_write_byte_data(i2c, pin, address, MCP_REG_GPPUB, 0xF)
 
 if __name__ == "__main__":
     main()
