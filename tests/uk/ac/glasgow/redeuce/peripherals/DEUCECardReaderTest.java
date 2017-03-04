cc9d424ef84d700de86d8ae33528880c94c35fda
Blair Archibald <mrblairarchibald@gmail.com>
2017-03-04 11:48:32 +0000

    Fix resource paths for test cards

--- tests/uk/ac/glasgow/redeuce/peripherals/DEUCECardReaderTest.java
+++ tests/uk/ac/glasgow/redeuce/peripherals/DEUCECardReaderTest.java
@@ -10,7 +10,7 @@ public class DEUCECardReaderTest {
 
 	@Test
 	public void getDelayLineTest() throws IOException, OutOfCardsException {
-		String file = "/users/level3/2089278r/INTERN/REDEUCE/src/uk/ac/glasgow/redeuce/peripherals/prog07DH.crd";
+		String file = "./cards/prog07DH.crd";
 		CRDFileReader testReader = new CRDFileReader(file);
 		FixedCardDeck newDeck = testReader.createNewDeck();
 		DEUCECardReader reader = new DEUCECardReader();
@@ -22,7 +22,7 @@ public class DEUCECardReaderTest {
 	
 	@Test
 	public void takeInCardsTest() throws OutOfCardsException, IOException{
-		String file = "/users/level3/2089278r/INTERN/REDEUCE/src/uk/ac/glasgow/redeuce/peripherals/prog07DH.crd";
+		String file = "./cards/prog07DH.crd";
 		CRDFileReader testReader = new CRDFileReader(file);
 		FixedCardDeck newDeck = testReader.createNewDeck();
 		DEUCECardReader reader = new DEUCECardReader();
@@ -33,7 +33,7 @@ public class DEUCECardReaderTest {
 	
 	@Test
 	public void readFirstCardTest() throws OutOfCardsException, IOException{
-		String file = "/users/level3/2089278r/INTERN/REDEUCE/src/uk/ac/glasgow/redeuce/peripherals/prog07DH.crd";
+		String file = "./cards/prog07DH.crd";
 		CRDFileReader testReader = new CRDFileReader(file);
 		FixedCardDeck newDeck = testReader.createNewDeck();
 		DEUCECardReader reader = new DEUCECardReader();
